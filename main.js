547f6a11c028f21a05eaee977097076c8ec3d0f5
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-27 10:21:19 +0900

    섹션2에 절반정도 보일때 모션 적용

--- main.js
+++ main.js
@@ -6,16 +6,22 @@
 const secs = document.querySelectorAll('section');
 const h1 = secs[0].querySelector('h1');
 const h1_2 = secs[1].querySelector('h1');
+//baseLine은 박스의 스크롤 모션이 시작되는 위치값을 기존 박스영역에서 300만큼 위로 올림
+const baseLine = -300;
 
 window.addEventListener('scroll', () => {
 	const scroll = window.scrollY;
-	const scroll_2 = scroll - secs[1].offsetTop;
+	//박스의 실적용되는 스크롤 수치값은 반대로 300만큼 더해줘야함
+	const scroll_2 = scroll - secs[1].offsetTop - baseLine;
 
-	h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${1 + scroll / 300})`;
-	h1.style.opacity = 1 - scroll / 600;
-
-	h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${
+	h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${
 		1 + scroll / 300
 	})`;
-	h1_2.style.opacity = 1 - scroll_2 / 600;
+	h1.style.opacity = 1 - scroll / 600;
+
+	if (scroll >= secs[1].offsetTop + baseLine) {
+		h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${
+			1 + scroll_2 / 200
+		})`;
+	}
 });
