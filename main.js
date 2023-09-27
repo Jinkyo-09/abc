ba87b271e3e811491071194a1165221f0b558d61
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-27 13:39:57 +0900

    애니메 기본 사용 추가

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const box = document.querySelector('article');
 box.addEventListener('click', () => {
 	new Anime(
 		box, //선택자
-		{ left: 300, backgroundColor: '#ff5a75', width: 400 }, //모션 적용할 속성값
-		{ duration: 1000, easeType: 'ease1' }
+		{ left: 600, backgroundColor: '#ff5a75' }, //모션 적용할 속성값
+		{
+			duration: 1000,
+			easeType: 'linear',
+			callback: () => {
+				new Anime(box, { top: 300, background: '#add8e6' });
+			},
+		}
 	);
-	//duration:시간, easeType:가속도,callback:첫번째 모션이 끝나고 이어서 실행할 콜백 함수
 });
