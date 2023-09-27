4938ad25592e6dbfd81a7652cb4ca6d16d6c9b53
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-27 10:44:01 +0900

    3항 연산자 에러 수정

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ const secs = document.querySelectorAll('section');
 const h1 = secs[0].querySelector('h1');
 const h1_2 = secs[1].querySelector('h1');
 //baseLine은 박스의 스크롤 모션이 시작되는 위치값을 기존 박스영역에서 300만큼 위로 올림
-const baseLine = -300;
+const baseLine = window.innerHeight / 2;
 
 window.addEventListener('scroll', () => {
 	const scroll = window.scrollY;
 	//박스의 실적용되는 스크롤 수치값은 반대로 300만큼 더해줘야함
-	const scroll_2 = scroll - secs[1].offsetTop - baseLine;
 	const initScroll = 0;
 
 	h1.style.transform = `translateX(${scroll}px) rotate(${scroll}deg) scale(${
@@ -20,10 +19,12 @@ window.addEventListener('scroll', () => {
 	})`;
 	h1.style.opacity = 1 - scroll / 600;
 
-	let Scroll_2 = 0;
+	let scroll_2 = 0;
 
+	//스크롤거리값이 2번째 섹션영역에 들어가면 scroll_2에는 scroll적용되는 값을 담고
+	//반대로 2번째 섹션영역을 벗어나면 scroll_2의 값을 0으로 초기화
 	scroll >= secs[1].offsetTop + baseLine
-		? scroll >= secs[1].offsetTop + baseLine
+		? (scroll_2 = scroll - secs[1].offsetTop - baseLine)
 		: (scroll_2 = 0);
 
 	h1_2.style.transform = `translateX(${scroll_2}px) rotate(${scroll_2}deg) scale(${
